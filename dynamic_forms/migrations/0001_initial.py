3aa8e5713012996050af15615e143130c65889b0
Nar Kumar <neokya@gmail.com>
2013-11-20 00:42:31 +0530

    add new migration file

--- dynamic_forms/migrations/0001_initial.py
+++ dynamic_forms/migrations/0001_initial.py
@@ -13,21 +13,21 @@ class Migration(SchemaMigration):
             (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
             ('name', self.gf('django.db.models.fields.CharField')(unique=True, max_length=50)),
             ('submit_url', self.gf('django.db.models.fields.CharField')(unique=True, max_length=100)),
-            ('success_url', self.gf('django.db.models.fields.CharField')(default='', max_length=100, blank=True)),
-            ('actions', self.gf('dynamic_forms.fields.TextMultiSelectField')(default='')),
-            ('form_template', self.gf('django.db.models.fields.CharField')(max_length=100)),
-            ('success_template', self.gf('django.db.models.fields.CharField')(max_length=100)),
+            ('success_url', self.gf('django.db.models.fields.CharField')(default=u'', max_length=100, blank=True)),
+            ('actions', self.gf('dynamic_forms.fields.TextMultiSelectField')(default=u'')),
+            ('form_template', self.gf('django.db.models.fields.CharField')(default=u'dynamic_forms/form.html', max_length=100)),
+            ('success_template', self.gf('django.db.models.fields.CharField')(default=u'dynamic_forms/form_success.html', max_length=100)),
         ))
         db.send_create_signal(u'dynamic_forms', ['FormModel'])
 
         # Adding model 'FormFieldModel'
         db.create_table(u'dynamic_forms_formfieldmodel', (
             (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('parent_form', self.gf('django.db.models.fields.related.ForeignKey')(related_name='fields', to=orm['dynamic_forms.FormModel'])),
+            ('parent_form', self.gf('django.db.models.fields.related.ForeignKey')(related_name=u'fields', to=orm['dynamic_forms.FormModel'])),
             ('field_type', self.gf('django.db.models.fields.CharField')(max_length=255)),
             ('label', self.gf('django.db.models.fields.CharField')(max_length=20)),
             ('name', self.gf('django.db.models.fields.SlugField')(max_length=50, blank=True)),
-            ('_options', self.gf('django.db.models.fields.TextField')(default='{}', blank=True)),
+            ('_options', self.gf('django.db.models.fields.TextField')(null=True, blank=True)),
             ('position', self.gf('django.db.models.fields.SmallIntegerField')(default=0, blank=True)),
         ))
         db.send_create_signal(u'dynamic_forms', ['FormFieldModel'])
@@ -38,9 +38,9 @@ class Migration(SchemaMigration):
         # Adding model 'FormModelData'
         db.create_table(u'dynamic_forms_formmodeldata', (
             (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('form', self.gf('django.db.models.fields.related.ForeignKey')(related_name='data', null=True, on_delete=models.SET_NULL, to=orm['dynamic_forms.FormModel'])),
-            ('value', self.gf('django.db.models.fields.TextField')(default='', blank=True)),
-            ('submitted', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime.now)),
+            ('form', self.gf('django.db.models.fields.related.ForeignKey')(related_name=u'data', null=True, on_delete=models.SET_NULL, to=orm['dynamic_forms.FormModel'])),
+            ('value', self.gf('djorm_hstore.fields.DictionaryField')(default=u'', blank=True)),
+            ('submitted', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
         ))
         db.send_create_signal(u'dynamic_forms', ['FormModelData'])
 
@@ -61,31 +61,31 @@ class Migration(SchemaMigration):
 
     models = {
         u'dynamic_forms.formfieldmodel': {
-            'Meta': {'ordering': "['position']", 'unique_together': "(('parent_form', 'name'),)", 'object_name': 'FormFieldModel'},
+            'Meta': {'ordering': "[u'parent_form', u'position']", 'unique_together': "((u'parent_form', u'name'),)", 'object_name': 'FormFieldModel'},
+            '_options': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
             'field_type': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'label': ('django.db.models.fields.CharField', [], {'max_length': '20'}),
             'name': ('django.db.models.fields.SlugField', [], {'max_length': '50', 'blank': 'True'}),
-            '_options': ('django.db.models.fields.TextField', [], {'default': "'{}'", 'blank': 'True'}),
-            'parent_form': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'fields'", 'to': u"orm['dynamic_forms.FormModel']"}),
+            'parent_form': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "u'fields'", 'to': u"orm['dynamic_forms.FormModel']"}),
             'position': ('django.db.models.fields.SmallIntegerField', [], {'default': '0', 'blank': 'True'})
         },
         u'dynamic_forms.formmodel': {
-            'Meta': {'ordering': "['name']", 'object_name': 'FormModel'},
-            'actions': ('dynamic_forms.fields.TextMultiSelectField', [], {'default': "''"}),
-            'form_template': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'Meta': {'ordering': "[u'name']", 'object_name': 'FormModel'},
+            'actions': ('dynamic_forms.fields.TextMultiSelectField', [], {'default': "u''"}),
+            'form_template': ('django.db.models.fields.CharField', [], {'default': "u'dynamic_forms/form.html'", 'max_length': '100'}),
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '50'}),
             'submit_url': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '100'}),
-            'success_template': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'success_url': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '100', 'blank': 'True'})
+            'success_template': ('django.db.models.fields.CharField', [], {'default': "u'dynamic_forms/form_success.html'", 'max_length': '100'}),
+            'success_url': ('django.db.models.fields.CharField', [], {'default': "u''", 'max_length': '100', 'blank': 'True'})
         },
         u'dynamic_forms.formmodeldata': {
             'Meta': {'object_name': 'FormModelData'},
-            'form': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'data'", 'null': 'True', 'on_delete': 'models.SET_NULL', 'to': u"orm['dynamic_forms.FormModel']"}),
+            'form': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "u'data'", 'null': 'True', 'on_delete': 'models.SET_NULL', 'to': u"orm['dynamic_forms.FormModel']"}),
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'submitted': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
-            'value': ('django.db.models.fields.TextField', [], {'default': "''", 'blank': 'True'})
+            'submitted': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'value': ('djorm_hstore.fields.DictionaryField', [], {'default': "u''", 'blank': 'True'})
         }
     }
 
